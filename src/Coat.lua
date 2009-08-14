c0350892b9238edaa7fa5b38617cd9756d10ae01
Francois Perrad <francois.perrad@gadz.org>
2009-08-14 16:14:44 +0200

    prepare release 0.2.0

--- src/Coat.lua
+++ src/Coat.lua
@@ -588,7 +588,7 @@ function _G.class (modname)
     classes[modname] = M
 end
 
-_VERSION = "0.1.0"
+_VERSION = "0.2.0"
 _DESCRIPTION = "lua-Coat : Yet Another Lua Object-Oriented Model"
 _COPYRIGHT = "Copyright (c) 2009 Francois Perrad"
 --
